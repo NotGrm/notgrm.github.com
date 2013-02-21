bd30543f66e29545d8b09805f641be2ac0e98d75
NotGrm <notgrm@gmail.com>
2013-02-21 21:25:56 +0100

    First commit

---
+++ helpers/site_helpers.rb
@@ -0,0 +1,20 @@
+module SiteHelpers
+
+  def page_title
+    title = "NotGrm"
+    if data.page.title
+      title << " | " + data.page.title
+    end
+    title
+  end
+  
+  def page_description
+    if data.page.description
+      description = data.page.description
+    else
+      description = "Set your site description in /helpers/site_helpers.rb"
+    end
+    description
+  end
+
+end
